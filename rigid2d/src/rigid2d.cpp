4ba0402c3c4ba4566ef59cde358ab070b32ef222
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-01-22 18:49:32 -0600

    add comments file

--- rigid2d/src/rigid2d.cpp
+++ rigid2d/src/rigid2d.cpp
@@ -67,46 +67,81 @@ double rhs_x = rhs.trans_vector.x;
 double rhs_y = rhs.trans_vector.y;
 double rhs_angle = rhs.trans_angle;
 
-double lhs_x = this->trans_vector.x;
-double lhs_y = this->trans_vector.y;
-double lhs_angle = this->trans_angle;
+double lhs_x = trans_vector.x;
+double lhs_y = trans_vector.y;
+double lhs_angle = trans_angle;
 
-Transform2D new_trans;
-new_trans.trans_vector.x = lhs_x + rhs_x * cos(lhs_angle) - rhs_y * sin(lhs_angle);
-new_trans.trans_vector.y = lhs_y + rhs_x * sin(lhs_angle) + rhs_y * cos(lhs_angle);
-new_trans.trans_angle = lhs_angle + rhs_angle;
+trans_vector.x = lhs_x + rhs_x * cos(lhs_angle) - rhs_y * sin(lhs_angle);
+trans_vector.y = lhs_y + rhs_x * sin(lhs_angle) + rhs_y * cos(lhs_angle);
+trans_angle = lhs_angle + rhs_angle;
 
-// Vector2D sol_trans;
-// double sol_angle;
-// sol_trans.x = lhs_x + rhs_x * cos(lhs_angle) - rhs_y * sin(lhs_angle);
-// sol_trans.y = lhs_y + rhs_x * sin(lhs_angle) + rhs_y * cos(lhs_angle);
-// sol_angle = lhs_angle + rhs_angle;
-// Transform2D new_trans = Transform2D(sol_trans, sol_angle);
-
-return new_trans;
+return *this;
 }
+
+
+
+
+// /// \brief output a 2 dimensional vector as [xcomponent ycomponent]
+// /// os - stream to output to
+// /// v - the vector to print
+// std::ostream & operator<<(std::ostream & os, const Vector2D & v){
+//     os << '[' << v.x << ' ' << v.y << ']';
+//     return os;
+// }
+
+/// \brief input a 2 dimensional vector
+///   You should be able to read vectors entered as two numbers
+///   separated by a newline or a space, or entered as [xcomponent, ycomponent]
+/// is - stream from which to read
+/// v [out] - output vector
+/// Hint: The following may be useful:
+/// https://en.cppreference.com/w/cpp/io/basic_istream/peek
+/// https://en.cppreference.com/w/cpp/io/basic_istream/get
+std::istream & operator>>(std::istream & is, Vector2D & v){
+    // double x,y;
+    char c1 = is.peek();
+    char c2 = is.get();
+
+    // is >> x >> y;
+
+    is >> v.x;
+    is >> v.y;
+    return is;
+}
+
 
 
 // /// \brief \see operator<<(...) (declared outside this class)
 // /// for a description
-// friend std::ostream & Transform2D::operator<<(std::ostream & os, const Transform2D & tf);
+// friend std::ostream & Transform2D::operator<<(std::ostream & os, const Transform2D & tf){
+//     os << '[' << tf.trans_vector.x << ' ' << tf.trans_vector.y << ' ' << tf.trans_angle << ']';
+//     return os;
+// }
 
 // /// \brief should print a human readable version of the transform:
 // /// An example output:
 // /// dtheta (degrees): 90 dx: 3 dy: 5
 // /// \param os - an output stream
 // /// \param tf - the transform to print
-// std::ostream & Transform2D::operator<<(std::ostream & os, const Transform2D & tf);
+// std::ostream & operator<<(std::ostream & os, const Transform2D & tf){
+//     os << 'dtheta (degrees): ' << tf.trans_vector.x << ', dx: ' << tf.trans_vector.y << ', dy: ' << tf.trans_angle << '.';
+//     return os;
+// }
 
 // /// \brief Read a transformation from stdin
 // /// Should be able to read input either as output by operator<< or
 // /// as 3 numbers (degrees, dx, dy) separated by spaces or newlines
-// std::istream & Transform2D::operator>>(std::istream & is, Transform2D & tf);
+// std::istream & operator>>(std::istream & is, Transform2D & tf){
+//     is >> tf.trans_vector.x;
+//     is >> tf.trans_vector.y;
+//     is >> tf.trans_angle;
+//     return is;
+// }
 
 // /// \brief multiply two transforms together, returning their composition
 // /// \param lhs - the left hand operand
 // /// \param rhs - the right hand operand
 // /// \return the composition of the two transforms
 // /// HINT: This function should be implemented in terms of *=
-// Transform2D Transform2D::operator*(Transform2D lhs, const Transform2D & rhs);
+// Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
 
