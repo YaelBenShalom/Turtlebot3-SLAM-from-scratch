4fd99ed499ee92bca40fac52d11b0a2c113727ef
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-01-27 17:40:14 -0600

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yael/Documents/MSR_Courses/Winter_2021/ME495-Sensing_Navigation_and_ML_for_Robotics/ME495ws/src/assignment-git/rigid2d
-# Build directory: /home/yael/Documents/MSR_Courses/Winter_2021/ME495-Sensing_Navigation_and_ML_for_Robotics/ME495ws/src/assignment-git/rigid2d/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test_of_Rigid2d "/home/yael/Documents/MSR_Courses/Winter_2021/ME495-Sensing_Navigation_and_ML_for_Robotics/ME495ws/src/assignment-git/rigid2d/build/rigid_test")
-set_tests_properties(Test_of_Rigid2d PROPERTIES  _BACKTRACE_TRIPLES "/home/yael/Documents/MSR_Courses/Winter_2021/ME495-Sensing_Navigation_and_ML_for_Robotics/ME495ws/src/assignment-git/rigid2d/CMakeLists.txt;16;add_test;/home/yael/Documents/MSR_Courses/Winter_2021/ME495-Sensing_Navigation_and_ML_for_Robotics/ME495ws/src/assignment-git/rigid2d/CMakeLists.txt;0;")
