856dd2d4878b8250d988854f448b681dc1e34575
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-02-03 12:58:17 -0600

    Turned the rigid2d library into a ROS package

--- rigid2d/tests/tests.cpp
+++ rigid2d/tests/tests.cpp
@@ -1,12 +1,13 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
+// #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main()
 
-#include "catch.hpp"
 #include "../include/rigid2d/rigid2d.hpp"
+#include "../src/rigid2d.cpp"
 
 #include <cmath>
 #include <iostream>
 #include <sstream>
-#include <gtest/gtest.h>
+#include <catch_ros/catch.hpp>
+
 
 TEST_CASE( "Test function almost_equal(d1, d2, epsilon)" ) {
 
