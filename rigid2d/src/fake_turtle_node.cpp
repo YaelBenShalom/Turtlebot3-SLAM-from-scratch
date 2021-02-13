f54c4acedce5d2d6b186fa9d36dccf3a556e11eb
Yael Ben Shalom <yael.bshalom@gmail.com>
2021-02-13 13:41:32 -0600

    joint_state msg is workning

--- rigid2d/src/fake_turtle_node.cpp
+++ rigid2d/src/fake_turtle_node.cpp
@@ -45,8 +45,8 @@ class FakeTurtle
             load_parameter();
 
             // Init publishers, subscribers, and services
-            joint_states_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 5);
-            vel_sub = nh.subscribe("turtle1/cmd_vel", 10, &FakeTurtle::cmd_vel_callback, this);
+            joint_states_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
+            vel_sub = nh.subscribe("cmd_vel", 10, &FakeTurtle::cmd_vel_callback, this);
          }
 
         /// \brief Load the parameters from the parameter server
@@ -68,9 +68,9 @@ class FakeTurtle
             twist.ydot = tw.linear.y / (double)frequency;
 
             wheel_angle = diff_drive.updateOdometryWithTwist(twist);
-
             // Raise the cmd_vel flag
             cmd_vel_flag = true;
+            
         }
 
 
@@ -80,18 +80,20 @@ class FakeTurtle
             ROS_INFO("Entering the loop");
             ros::Rate loop_rate(frequency);
             while(ros::ok()) {
-                ROS_INFO("Looping");
+                // ROS_INFO("Looping");
                 current_time = ros::Time::now();
                 
                 // If the cmd_vel_callback was called
                 if (cmd_vel_flag) {
+                    sensor_msgs::JointState joint_state;
+
+                    // ROS_INFO("Entering if statement/n");
                     wheel_angle = diff_drive.get_wheel_angle();
                     joint_state.header.stamp = current_time;
 
                     joint_state.name.push_back(right_wheel_joint);
-                    joint_state.position.push_back(wheel_angle.right_wheel_angle);
-
                     joint_state.name.push_back(left_wheel_joint);
+                    joint_state.position.push_back(wheel_angle.right_wheel_angle);
                     joint_state.position.push_back(wheel_angle.left_wheel_angle);
 
                     joint_states_pub.publish(joint_state);
@@ -99,7 +101,6 @@ class FakeTurtle
                     // Remove the cmd_vel flag
                     cmd_vel_flag = false;
                 }
-
                 loop_rate.sleep();
                 ros::spinOnce();
             }
@@ -116,7 +117,7 @@ class FakeTurtle
         ros::Subscriber vel_sub;
         ros::Time current_time;
 
-        sensor_msgs::JointState joint_state;
+        // sensor_msgs::JointState joint_state;
 
         rigid2d::Config2D pose;
         rigid2d::Twist2D twist;
@@ -129,7 +130,6 @@ class FakeTurtle
 int main(int argc, char * argv[])
 {
     ros::init(argc, argv, "fake_turtle_node");
-    
     FakeTurtle node;
     node.main_loop();
     ros::spin();
